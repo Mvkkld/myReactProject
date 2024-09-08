462719ae7f6c32bce59d083061b124602b048b5e
Mvkkld <153894558+Mvkkld@users.noreply.github.com>
2024-09-08 17:50:10 +0500

    lashpash

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -6,10 +6,7 @@ function App() {
     <> 
      <h1 className="text-3xl font-bold underline">
       Hello world!
-    </h1>
-    <h2>Pakistan</h2>
-    <h3>Zindabad</h3>
-    <h4>ye bhi commit hogya hy</h4>
+    </h1>    
     </>
   )
 }
